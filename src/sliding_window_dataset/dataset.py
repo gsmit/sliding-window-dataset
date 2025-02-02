d4de6ca086d23b7fb76811b548d8d7347e82775d
gsmit <gijs.smit@outlook.com>
2025-02-02 15:40:09 +0100

    Support multiple time series

--- src/sliding_window_dataset/dataset.py
+++ src/sliding_window_dataset/dataset.py
@@ -1,23 +1,35 @@
+from typing import Callable, Optional, Sequence
+
 from torch.utils.data import Dataset
 
 
 class SlidingWindowDataset(Dataset):
-    """A dataset that iterates over data using a sliding window."""
+    """A dataset that iterates over time series data using a sliding window."""
 
-    def __init__(self, *data, window: int):
+    def __init__(
+        self,
+        *data: Sequence,
+        window_size: int,
+        transform: Optional[Callable[[Sequence], Sequence]] = None,
+    ):
         if not data:
-            raise ValueError("At least one iterable must be provided.")
+            raise ValueError("At least one time series must be provided.")
 
-        length = len(data[0])
-        if any(len(d) != length for d in data):
-            raise ValueError("All iterables must have the same length.")
+        if not all(len(series) == len(data[0]) for series in data):
+            raise ValueError("All time series must have the same length.")
+
+        if len(data[0]) < window_size:
+            raise ValueError("Time series cannot be smaller than window.")
 
         self.data = data
-        self.window = window
-        self.size = length - window
+        self.window_size = window_size
+        self.transform = transform
+        self.n_windows = len(data[0]) - window_size + 1
 
-    def __getitem__(self, index):
-        return tuple(d[index : index + self.window] for d in self.data)
+    def __len__(self) -> int:
+        return len(self.data) * self.n_windows
 
-    def __len__(self):
-        return self.size
+    def __getitem__(self, idx: int) -> Sequence:
+        series, timestep = divmod(idx, self.n_windows)
+        window = self.data[series][timestep : timestep + self.window_size]
+        return self.transform(window) if self.transform else window
